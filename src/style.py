b77420db5e990ac390ac87a56fb3648e995569f6
Greg Rafferty <greg.rafferty@gmail.com>
2018-01-05 15:34:33 -0800

    adds avg lengths columns to dataframe

--- src/style.py
+++ src/style.py
@@ -18,3 +18,18 @@ def sentence_word_length(text):
         sentence_lengths.append(sentence_length)
     return (sum(sentence_lengths) / float(len(sentence_lengths)),
             sum(word_lengths) / float(len(word_lengths)))
+
+
+def apply_avg_lengths(df, column):
+    '''
+    takes a DataFrame with a specified column of text and adds two new columns
+    to the DataFrame, corresponding to the average sentence and word lengths
+    INPUT: DataFrame, string
+    OUTPUT: the original DataFrame with two additional columns
+    '''
+
+    avg_lengths = pd.DataFrame(df[column].apply(sentence_word_length))
+    unpacked = pd.DataFrame([d for idx, d in avg_lengths[column].iteritems()],
+                            index=avg_lengths.index)
+    unpacked.columns = ['avg_sentence_length', 'avg_word_length']
+    return pd.concat([df, unpacked], axis=1)
