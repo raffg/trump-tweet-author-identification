af20690aab57f095d5e3e38eba02e4df6eb3d78b
Greg Rafferty <greg.rafferty@gmail.com>
2018-01-06 12:23:04 -0800

    created tweetstorm function

---
+++ src/tweetstorm.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+
+def tweetstorm(df, tweet, source, timestamp, time_threshold):
+    '''
+    takes a DataFrame with a specified column containing tweets, a specified
+    column identifying the source of the tweet, a specified column indicating
+    the timestamp of the tweet, and a threshold in seconds defining the
+    maximimum time which can pass between tweets to define a tweetstorm
+    INPUT: DataFrame, string, string, string, int
+    OUTPUT: the original DataFrame with one new column
+    '''
+
+    temp = pd.DataFrame()
+    df = df.copy()
+    temp['time_diff'] = df.groupby(source)[timestamp].diff().dt.total_seconds()
+    temp['time_diff_prev'] = temp['time_diff'].shift(-1)
+    df['tweetstorm'] = temp.eval('time_diff < @time_threshold | \
+                                 time_diff2 < @time_threshold')
+    return df
