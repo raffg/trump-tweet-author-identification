65a2dc06e9ed32c588fd09f1696bc5847ae418e0
Greg Rafferty <greg.rafferty@gmail.com>
2018-01-22 16:14:37 -0800

    added gaussian naive bayes to ensemble

---
+++ gaussian_naive_bayes.py
@@ -0,0 +1,85 @@
+import pandas as pd
+import numpy as np
+import pickle
+from sklearn.naive_bayes import GaussianNB
+from sklearn.decomposition import PCA
+from sklearn.metrics import accuracy_score, precision_score, recall_score
+
+
+def main():
+    run_model_gaussian_nb()
+
+
+def run_model_gaussian_nb():
+    X_train = pd.read_pickle('pickle/train_all_std.pkl')
+    X_val = pd.read_pickle('pickle/test_all_std.pkl')
+    y_train = pd.read_pickle('pickle/y_train_all_std.pkl')
+    y_val = pd.read_pickle('pickle/y_test_all_std.pkl')
+
+    feat = ['favorite_count', 'is_retweet', 'retweet_count', 'is_reply',
+            'compound', 'v_negative', 'v_neutral', 'v_positive', 'anger',
+            'anticipation', 'disgust', 'fear', 'joy', 'negative', 'positive',
+            'sadness', 'surprise', 'trust', 'tweet_length',
+            'avg_sentence_length', 'avg_word_length', 'commas',
+            'semicolons', 'exclamations', 'periods', 'questions', 'quotes',
+            'ellipses', 'mentions', 'hashtags', 'urls', 'is_quoted_retweet',
+            'all_caps', 'tweetstorm', 'hour', 'hour_20_02', 'hour_14_20',
+            'hour_08_14', 'hour_02_08', 'start_mention']
+
+    drop = ['created_at', 'id_str', 'in_reply_to_user_id_str', 'tweetokenize',
+            'text', 'pos', 'ner']
+
+    whole_train = X_train.drop(drop, axis=1)
+    whole_val = X_val.drop(drop, axis=1)
+
+    top_feat = set(np.load('pickle/top_features.npz')['arr_0'][:13])
+    train_feat = []
+    val_feat = []
+    for feat in top_feat:
+        if feat in whole_train.columns:
+            train_feat.append(feat)
+        if feat in whole_val.columns:
+            val_feat.append(feat)
+
+    pca = PCA(n_components=10)
+    pca.fit(whole_train[train_feat])
+    whole_train = pca.transform(whole_train[train_feat])
+    whole_val = pca.transform(whole_val[val_feat])
+
+    print('condensed model')
+    condensed_train = whole_train
+    condensed_val = whole_val
+    gaussian_nb_condensed = gaussian_nb(np.array(condensed_train),
+                                        np.array(condensed_val),
+                                        np.array(y_train).ravel(),
+                                        np.array(y_val).ravel())
+    gaussian_nb_save_pickle(gaussian_nb_condensed)
+
+
+def gaussian_nb(X_train, X_val, y_train, y_val):
+    # Gaussian Naive Bayes
+    nb = GaussianNB().fit(X_train, y_train)
+    predicted = nb.predict(X_val)
+    accuracy_train = np.mean(nb.predict(X_train) == y_train)
+    accuracy_test = np.mean(predicted == y_val)
+
+    print('Accuracy: ', accuracy_score(y_val, predicted))
+    print('Precision: ', precision_score(y_val, predicted))
+    print('Recall: ', recall_score(y_val, predicted))
+    print()
+
+    return nb
+
+
+def gaussian_nb_save_pickle(model):
+    # Save pickle file
+    output = open('pickle/gaussian_nb_model.pkl', 'wb')
+    print('Pickle dump model')
+    pickle.dump(model, output, protocol=4)
+    output.close()
+
+    return
+
+
+if __name__ == '__main__':
+    main()
--- predict_results.py
+++ predict_results.py
@@ -29,6 +29,9 @@ def predict_tweet(created_at):
     with open('pickle/naive_bayes_model.pkl', 'rb') as nb_f:
         nb = pickle.load(nb_f)
 
+    with open('pickle/gaussian_nb_model.pkl', 'rb') as gnb_f:
+        gnb = pickle.load(gnb_f)
+
     with open('pickle/adaboost_model.pkl', 'rb') as ab_f:
         ab = pickle.load(ab_f)
 
@@ -56,6 +59,7 @@ def predict_tweet(created_at):
     rf_feat = np.load('pickle/top_features.npz')['arr_0'][:200]
     lr_feat = np.load('pickle/top_features.npz')['arr_0'][:200]
     nb_feat = np.load('pickle/top_features.npz')['arr_0'][:5]
+    gnb_feat = np.load('pickle/top_features.npz')['arr_0'][:13]
     knn_feat = np.load('pickle/top_features.npz')['arr_0'][:13]
     svm_feat = np.load('pickle/top_features.npz')['arr_0'][:300]
     ab_feat = np.load('pickle/top_features.npz')['arr_0'][:300]
@@ -72,33 +76,39 @@ def predict_tweet(created_at):
     tweet = tweet.drop(drop, axis=1)
     tweet_std = tweet_std.drop(drop, axis=1)
     tweet_knn = tweet_std
+    tweet_gnb = tweet_std
 
     knn_train = pd.read_pickle('pickle/train_all_std.pkl')
     knn_train = knn_train.drop(drop, axis=1)
     pca = PCA(n_components=12)
     pca.fit(knn_train[knn_feat])
-
     tweet_knn = pca.transform(tweet_knn[knn_feat])
+
+    pca = PCA(n_components=10)
+    pca.fit(knn_train[gnb_feat])
+    tweet_gnb = pca.transform(tweet_gnb[gnb_feat])
 
     tweet_rf = rf.predict(tweet[rf_feat])
     tweet_ab = ab.predict(tweet_std[ab_feat])
     tweet_knn = knn.predict(tweet_knn)
     tweet_nb = nb.predict(tweet_std[nb_feat])
+    tweet_gnb = gnb.predict(tweet_gnb)
     tweet_svm = svm.predict(tweet_std[svm_feat])
     tweet_lr = lr.predict(tweet_std[lr_feat])
     proba_lr = lr.predict_proba(tweet_std[lr_feat])
 
-    maj_list = [2 * tweet_rf[0], tweet_svm[0], tweet_lr[0],
+    maj_list = [tweet_rf[0], tweet_svm[0], tweet_lr[0], tweet_gnb[0],
                 tweet_nb[0], tweet_ab[0], tweet_knn[0]]
 
     majority = 1 if sum(maj_list) >= 4 else 0
 
-    print('Random Forest prediction:', tweet_rf)
-    print('AdaBoost prediction:', tweet_ab)
-    print('KNN prediction:', tweet_knn)
-    print('Naive Bayes prediction:', tweet_nb)
-    print('SVM prediction:', tweet_svm)
-    print('Logistic Regression prediction:', tweet_lr)
+    print('Random Forest prediction:         ', tweet_rf)
+    print('AdaBoost prediction:              ', tweet_ab)
+    print('KNN prediction:                   ', tweet_knn)
+    print('Naive Bayes prediction:           ', tweet_nb)
+    print('Gaussian Naive Bayes prediction:  ', tweet_gnb)
+    print('SVM prediction:                   ', tweet_svm)
+    print('Logistic Regression prediction:   ', tweet_lr)
     print('Logistic Regression probabilities:', proba_lr)
     print()
     print('Majority class:', majority)
